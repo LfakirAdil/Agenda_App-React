eed7c5e764a38e357c2279f7c2767196c26a9021
LFAKIR Adil <39201375+LfakirAdil@users.noreply.github.com>
2022-02-09 00:05:56 +0100

    Add files via upload

---
+++ src/constant/index.js
@@ -0,0 +1,21 @@
+import _ from 'lodash';
+
+export const weekArray = [
+    'Dim',
+    'Lun',
+    'Mar',
+    'Mer',
+    'Jeu',
+    'Ven',
+    'Sam'
+  ];
+
+export const gridArray = _.range(0, 42);
+
+export const yearOptions = [
+  '2021', '2022', '2023', '2024'
+];
+
+export const monthOptions = [
+  '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'
+]
